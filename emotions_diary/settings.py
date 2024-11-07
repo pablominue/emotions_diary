ae73e4fc27b75e20686fc89c877ebba46957682d
= <=>
2024-11-08 00:19:05 +0100

    New

--- emotions_diary/settings.py
+++ emotions_diary/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.128', '192.168.1.']
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'

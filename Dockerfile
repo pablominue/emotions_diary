92e537dda6be66765f79c9792db80beed055151d
= <=>
2024-11-08 10:31:06 +0100

    Nre

--- Dockerfile
+++ Dockerfile
@@ -6,19 +6,16 @@ ENV PYTHONPATH=/app
 
 WORKDIR /app
 
-# Copy the project files
-COPY . /app/
+COPY pyproject.toml poetry.lock /app/
 
-# Install dependencies
 RUN pip install poetry && \
     poetry config virtualenvs.create false && \
     poetry install --no-dev
 
-# Collect static files
+COPY . /app/
+
 RUN python manage.py collectstatic --noinput
 
-# Expose the port the app runs on
 EXPOSE 8000
 
-# Command to run the application
 CMD ["gunicorn", "--bind", "0.0.0.0:8000", "emotions_diary.wsgi:application"]
--- emotions_diary/settings.py
+++ emotions_diary/settings.py
@@ -19,7 +19,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
 if not DEBUG:
     # Configuraciones de seguridad para producción
-    SECURE_SSL_REDIRECT = True
+    SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
     SECURE_BROWSER_XSS_FILTER = True
@@ -36,11 +36,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -97,12 +93,12 @@ TEMPLATES = [
     },
 ]
 
-# STORAGES = {
-#     # ...
-#     "staticfiles": {
-#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-#     },
-# }
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 WSGI_APPLICATION = "emotions_diary.wsgi.application"
 
